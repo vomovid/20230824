2a49cc2ef6bc1b20c9ee904d44aec66e66651a59
vomovid <vomovid@gmail.com>
2023-08-24 21:50:23 +0900

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,14 @@
+import webbrowser
+import math
+url = 'https://docs.python.org/'
+
+num = math.sqrt(16)
+num2 = math.sqrt(25)
+num3 = math.sqrt(36)
+num4 = math.sqrt(49)
+num5 = math.sqrt(64)
+print(num)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
